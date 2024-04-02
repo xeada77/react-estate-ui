c57cd722dca7d85fdd84b950039f18a3caa3b695
xeada77 <luismartq@gmail.com>
2024-04-02 21:34:55 +0200

    CORS

--- src/lib/apiRequest.js
+++ src/lib/apiRequest.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiRequest = axios.create({
   baseURL: "http://localhost:8800/api",
-  withCredentials: false,
+  withCredentials: true,
 });
 
 export default apiRequest;
