1fe03949ef73b7fd3d77e70c87934ebadaac950d
xeada77 <luismartq@gmail.com>
2024-04-02 19:56:14 +0200

    connect to server

--- src/routes/login/login.jsx
+++ src/routes/login/login.jsx
@@ -1,23 +1,60 @@
+import { useState } from "react";
 import "./login.scss";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import apiRequest from "../../lib/apiRequest";
 
 const Login = () => {
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    setError("");
+
+    const formData = new FormData(e.target);
+
+    const username = formData.get("username");
+    const password = formData.get("password");
+
+    try {
+      const res = await apiRequest.post("/auth/login", {
+        username,
+        password,
+      });
+
+      localStorage.setItem("user", JSON.stringify(res.data));
+      console.log(res.data);
+      //navigate("/profile");
+    } catch (err) {
+      console.log(err.response.data);
+      setError(err.response.data.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <div className="register">
       <div className="formContainer">
-        <form>
-          <h1>Log In</h1>
+        <form onSubmit={handleSubmit}>
+          <h1>Welcome Back </h1>
           <input
             type="text"
             name="username"
             placeholder="Username"
+            required
           />
           <input
             type="password"
             name="password"
             placeholder="Password"
+            required
           />
-          <button>Register</button>
+          <button disabled={isLoading}>Login</button>
+          {error && <span>{error}</span>}
           <Link to="/register">Don´t you have an account?</Link>
         </form>
       </div>
--- src/routes/register/register.jsx
+++ src/routes/register/register.jsx
@@ -1,28 +1,77 @@
 import "./register.scss";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import apiRequest from "../../lib/apiRequest";
+import { useState } from "react";
 
 const Register = () => {
+  const [error, setError] = useState("");
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    setError("");
+
+    const formData = new FormData(e.target);
+
+    const username = formData.get("username");
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    try {
+      const res = await apiRequest.post(
+        "http://localhost:8800/api/auth/register",
+        {
+          username,
+          email,
+          password,
+        }
+      );
+
+      //console.log(res.data);
+      navigate("/login");
+    } catch (err) {
+      //console.log(err.response.data);
+      setError(err.response.data.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <div className="register">
       <div className="formContainer">
-        <form>
+        <form onSubmit={handleSubmit}>
           <h1>Create an Account</h1>
           <input
             type="text"
             name="username"
             placeholder="Username"
+            required
+            minLength={3}
+            maxLength={20}
           />
           <input
             type="text"
             name="email"
             placeholder="Email"
+            required
+            minLength={3}
+            maxLength={20}
           />
           <input
             type="password"
             name="password"
             placeholder="Password"
+            required
+            minLength={3}
+            maxLength={20}
           />
-          <button>Register</button>
+          <button disabled={isLoading}>Register</button>
+          {error && <span>{error}</span>}
           <Link to="/login">Do you have an account?</Link>
         </form>
       </div>
