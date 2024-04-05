acec94660386da3dddaa460cc0472dd8bee7b4b2
Luis Martinez <luismartinez@Mac-mini-de-Luis.local>
2024-04-05 15:03:02 +0200

    cloudinary

--- src/routes/profileUpdatePage/profileUpdatePage.jsx
+++ src/routes/profileUpdatePage/profileUpdatePage.jsx
@@ -1,13 +1,17 @@
 import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./profileUpdatePage.scss";
 import { AuthContext } from "../../Context/AuthContext";
 import apiRequest from "../../lib/apiRequest.js";
+import UploadWidget from "../../components/uploadWidget/uploadWidget.jsx";
 
 const ProfileUpdatePage = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
+  const navigate = useNavigate();
 
   const { currentUser, updateUser } = useContext(AuthContext);
+  const [avatar, setAvatar] = useState(currentUser.avatar);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -15,15 +19,17 @@ const ProfileUpdatePage = () => {
     const formData = new FormData(e.target);
 
     const { username, email, password } = Object.fromEntries(formData);
-    console.log(username);
+
     try {
       const res = await apiRequest.put(`/users/${currentUser.id}`, {
         username,
         email,
         password,
+        avatar,
       });
-      console.log(res.data);
+      //console.log(res.data);
       updateUser(res.data);
+      navigate("/profile");
     } catch (err) {
       console.log(err);
       setError(err.response.data.message);
@@ -74,8 +80,26 @@ const ProfileUpdatePage = () => {
       <div className="sideContainer">
         <img
           className="avatar"
-          src={currentUser.avatar || "/noavatar.png"}
+          src={avatar || "/noavatar.png"}
           alt=""
+        />
+        <UploadWidget
+          uwConfig={{
+            cloudName: "dwsetf6sr",
+            uploadPreset: "estateapp",
+            // cropping: true, //add a cropping step
+            // showAdvancedOptions: true,  //add advanced options (public_id and tag)
+            // sources: [ "local", "url"], // restrict the upload sources to URL and local files
+            multiple: false, //restrict upload to a single file
+            folder: "avatar", //upload files to the specified folder
+            // tags: ["users", "profile"], //add the given tags to the uploaded files
+            // context: {alt: "user_uploaded"}, //add the given context data to the uploaded files
+            clientAllowedFormats: ["png", "jpeg", "jpg", "gif", "wep"], //restrict uploading to image files only
+            maxImageFileSize: 2000000, //restrict file size to less than 2MB
+            // maxImageWidth: 2000, //Scales the image down to a width of 2000 pixels before uploading
+            // theme: "purple", //change to a purple theme
+          }}
+          setAvatar={setAvatar}
         />
       </div>
     </div>
